bc8e55fda488d8cdace15dec9d918e256384065e
Balaram Pratap <500061550@stu.upes.ac.in>
2018-03-20 01:06:48 +0530

    Single Inheitance

---
+++ Single_Inheritance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+class Vehicle {
+  public:
+    Vehicle()
+    {
+      cout << "This is a Vehicle" << endl;
+    }
+};
+class Car: public Vehicle{
+ 
+};
+ 
+int main()
+{   
+    Car obj;
+    return 0;
+}
