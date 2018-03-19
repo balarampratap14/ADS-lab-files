f779fa91138d06e42516c5999a2be432545862fc
Balaram Pratap <500061550@stu.upes.ac.in>
2018-03-20 00:48:35 +0530

    inheritance

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -6,14 +6,12 @@ class Parent
     public:
       int id_p;
 };
-  
-class Child : public Parent
+  class Child : public Parent
 {
     public:
       int id_c;
 };
- 
-int main() 
+ int main() 
    {
       
         Child obj1;
@@ -21,6 +19,5 @@ int main()
         obj1.id_p = 91;
         cout << "Child id is " <<  obj1.id_c << endl;
         cout << "Parent id is " <<  obj1.id_p << endl;
-         
         return 0;
    } 
