f804a9777472b0c102d6a8d470431a490d6a2cec
Balaram Pratap <500061550@stu.upes.ac.in>
2018-03-20 00:41:40 +0530

    files related to ads lab work

--- friend class.cpp
+++ friend class.cpp
@@ -1,31 +1,11 @@
 #include <iostream>
 using namespace std;
-class ONE;
-
-class TWO
-{
-public:
-  void print(ONE & x);
-};
-
-class ONE
-{
-  int a, b;
-  friend void TWO::print(ONE& x);
-public:
-  ONE() : a(1), b(2) { }
-};
-
-void TWO::print(ONE& x)
-{
-  cout << "a is " << x.a << endl;
-  cout << "b is " << x.b << endl;
-}
 
 int main()
 {
-  ONE xobj;
-  TWO yobj;
-  yobj.print(xobj);
+	int a=500,b=5420;
+	int c;
+	c=a&b;
+	cout<<"c"<<c;
 }
 
